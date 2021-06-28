ae02d8bb192bd47a5f8cdf36c84c44d60414eb6a
Erick Díaz <erickdp@hotmail.com>
2021-06-28 13:02:08 -0500

    Clases anidadas de Test con @Nested

--- src/test/java/org/ediaz/junit5app/ejemplo/models/CuentaTest.java
+++ src/test/java/org/ediaz/junit5app/ejemplo/models/CuentaTest.java
@@ -41,82 +41,95 @@ class CuentaTest {
         System.out.println("Finalizando la clase de prueba");
     }
 
-    //    El ciclo de vida es que se crea una insstancia de la clase para cada test
-    @Test
-    @DisplayName("Probando nombre de la cuenta corriente") // Esta anotacion sirve para dar nombre descriptivo al test
-    @Disabled
-    // Esta anotacion sirve para saltar las pruebas
-    void testNombreCuenta() {
+//    Las preubas se pueden categorizar para ordenarlas de mejor manera
+
+    @Nested
+    @DisplayName("Probando metodos de la cuenta corriente")
+    class CuentaSaldoYNombre {
+
+        //    El ciclo de vida es que se crea una insstancia de la clase para cada test
+        @Test
+        @DisplayName("Probando nombre de la cuenta corriente")
+        // Esta anotacion sirve para dar nombre descriptivo al test
+        @Disabled
+        // Esta anotacion sirve para saltar las pruebas
+        void testNombreCuenta() {
 //        fail(); Intenciona el fallo
 //        cuenta.setPersona("Erick");
-        var esperado = "Erick";
-        var real = cuenta.getPersona();
-        assertNotNull(real);
-        assertEquals(esperado, real); // se espera un valor y se lo compara con el que nos devuelva
-        assertTrue(real.equals("Erick"));
-    }
+            var esperado = "Erick";
+            var real = cuenta.getPersona();
+            assertNotNull(real);
+            assertEquals(esperado, real); // se espera un valor y se lo compara con el que nos devuelva
+            assertTrue(real.equals("Erick"));
+        }
 
-    @Test
-    @DisplayName("probando el saldo de la cuenta corriente, que no sea null, mayor que cero, valor esperado")
-    void testSaldoCuenta() {
+        @Test
+        @DisplayName("probando el saldo de la cuenta corriente, que no sea null, mayor que cero, valor esperado")
+        void testSaldoCuenta() {
 //        var cuenta = new Cuenta("Erick", new BigDecimal("1000.12345")); // Se puede usar solo new Cuenta y luego CTRL + ALT + V para generar codigo
-        assertNotNull(cuenta.getSaldo());
-        assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
-        assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0); // Si saldo es menor a 0 devuelve -1 si es mayor 1 y si son iguales 0
-        assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
-    }
+            assertNotNull(cuenta.getSaldo());
+            assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
+            assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0); // Si saldo es menor a 0 devuelve -1 si es mayor 1 y si son iguales 0
+            assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
+        }
 
-    @Test
-    @DisplayName("testeando referencias que sean iguales con el metodo equals")
-    void testReferenciaCuenta() {
-        var cuenta = new Cuenta("Jhon Doe", new BigDecimal("8900.9997"));
-        var cuenta2 = new Cuenta("Jhon Doe", new BigDecimal("8900.9997"));
+        @Test
+        @DisplayName("testeando referencias que sean iguales con el metodo equals")
+        void testReferenciaCuenta() {
+            var cuenta = new Cuenta("Jhon Doe", new BigDecimal("8900.9997"));
+            var cuenta2 = new Cuenta("Jhon Doe", new BigDecimal("8900.9997"));
 
 //        assertNotEquals(cuenta2, cuenta);
-        assertEquals(cuenta2, cuenta); // No son iguales por que tiene otro puntero a la memoria, pero si se agrega el metodo equals ya son iguales
+            assertEquals(cuenta2, cuenta); // No son iguales por que tiene otro puntero a la memoria, pero si se agrega el metodo equals ya son iguales
+        }
+
     }
 
-    @Test
-    void testDebitoCuenta() {
+    @Nested
+    class OperacionesCuenta {
+        @Test
+        void testDebitoCuenta() {
 //        var cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
-        cuenta.debito(new BigDecimal(100)); // De igual manera si se lanza una excepcion las pruebas de abajo no se ejecutan
-        assertNotNull(cuenta.getSaldo());
-        assertEquals(900, cuenta.getSaldo().intValue());
-        assertEquals("900.12345", cuenta.getSaldo().toPlainString());
-    }
+            cuenta.debito(new BigDecimal(100)); // De igual manera si se lanza una excepcion las pruebas de abajo no se ejecutan
+            assertNotNull(cuenta.getSaldo());
+            assertEquals(900, cuenta.getSaldo().intValue());
+            assertEquals("900.12345", cuenta.getSaldo().toPlainString());
+        }
 
-    @Test
-    void testCreditoCuenta() {
+        @Test
+        void testCreditoCuenta() {
 //        var cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
-        cuenta.credito(new BigDecimal(100));
-        assertNotNull(cuenta.getSaldo());
-        assertEquals(1100, cuenta.getSaldo().intValue());
-        assertEquals("1100.12345", cuenta.getSaldo().toPlainString());
-    }
+            cuenta.credito(new BigDecimal(100));
+            assertNotNull(cuenta.getSaldo());
+            assertEquals(1100, cuenta.getSaldo().intValue());
+            assertEquals("1100.12345", cuenta.getSaldo().toPlainString());
+        }
 
-    @Test
-    void testDineroInsuficienteException() {
+        @Test
+        void testDineroInsuficienteException() {
 //        var cuenta = new Cuenta("Erick", new BigDecimal("1000.12345"));
-        var excepcion = assertThrows(DineroInsuficienteException.class, () -> {
-            cuenta.debito(new BigDecimal(1500));
-        }); // Si se especifica aqui otra excepcion el test va a fallat porque la que se lanza es DineroInsuficienteException
-        var mensaje = excepcion.getMessage();
-        var esperado = "Dinero Insuficiente";
-        assertEquals(esperado, mensaje);
+            var excepcion = assertThrows(DineroInsuficienteException.class, () -> {
+                cuenta.debito(new BigDecimal(1500));
+            }); // Si se especifica aqui otra excepcion el test va a fallat porque la que se lanza es DineroInsuficienteException
+            var mensaje = excepcion.getMessage();
+            var esperado = "Dinero Insuficiente";
+            assertEquals(esperado, mensaje);
+        }
+
+        @Test
+        void testTransferirDineroCuentas() {
+            var cuenta1 = new Cuenta("Jhon Doe", new BigDecimal("2500"));
+            var cuenta2 = new Cuenta("Andres", new BigDecimal("1500.8989"));
+
+            var banco = new Banco();
+            banco.setNombre("Banco de Quito");
+            banco.transferir(cuenta2, cuenta1, new BigDecimal("500"));
+
+            assertEquals("1000.8989", cuenta2.getSaldo().toPlainString());
+            assertEquals("3000", cuenta1.getSaldo().toPlainString());
+        }
     }
 
-    @Test
-    void testTransferirDineroCuentas() {
-        var cuenta1 = new Cuenta("Jhon Doe", new BigDecimal("2500"));
-        var cuenta2 = new Cuenta("Andres", new BigDecimal("1500.8989"));
-
-        var banco = new Banco();
-        banco.setNombre("Banco de Quito");
-        banco.transferir(cuenta2, cuenta1, new BigDecimal("500"));
-
-        assertEquals("1000.8989", cuenta2.getSaldo().toPlainString());
-        assertEquals("3000", cuenta1.getSaldo().toPlainString());
-    }
 
     @Test
     @Disabled
@@ -149,64 +162,78 @@ class CuentaTest {
 
 //    Se utiliza anotaciones para definir si los metodos se ejecutan si cumple una propiedad como por ejemplo tipo de SO, arquitectura, etc.
 
-    @Test
-    @EnabledOnOs(OS.WINDOWS)
-    void testSoloWindows() {
+    @Nested
+    class TestSO {
+        @Test
+        @EnabledOnOs(OS.WINDOWS)
+        void testSoloWindows() {
+        }
+
+        @Test
+        @EnabledOnOs({OS.LINUX, OS.MAC})
+        void testSoloLinuxYMac() {
+        }
     }
 
-    @Test
-    @EnabledOnOs({OS.LINUX, OS.MAC})
-    void testSoloLinuxYMac() {
+    @Nested
+    class TestSystemProperties {
+        //    Si tiene un jdk diferente
+
+        @Test
+        void imprimirSystemProperties() {
+            var properties = System.getProperties();
+            properties.forEach((k, v) -> System.out.println(k + ":" + v));
+        }
+
+        @Test
+        @EnabledOnJre(JRE.JAVA_11)
+        void testJRE11() {
+        }
+
+        @Test
+        @DisabledOnJre(JRE.JAVA_16)
+        void testNOJRE15() {
+        }
+
+        @Test
+        @EnabledIfSystemProperty(named = "java.version", matches = "1.8.0")
+        void testJavaVersion() {
+        }
+
+        @Test
+        @EnabledIfSystemProperty(named = "user.name", matches = "peddoooo")
+        void testUsuario() {
+        }
+
+        @Test
+        @EnabledIfSystemProperty(named = "os.arch", matches = ".*64.*")
+        void testArquitectura() {
+        }
+
+        @Test
+        @EnabledIfSystemProperty(named = "ENV", matches = "dev")
+        void testDev() {
+        }
+
     }
 
-//    Si tiene un jdk diferente
+    @Nested
+    class TestVariablesEntorno {
+        @Test
+        void imprimirVariablesEntorno() {
+            var getenv = System.getenv();
+            getenv.forEach((k, v) -> System.out.println(k + ":" + v));
+        }
 
+        @Test
+        @EnabledIfEnvironmentVariable(named = "COMPUTERNAME", matches = "ASUS")
+        void testNombrePC() {
+        }
 
-    @Test
-    @EnabledOnJre(JRE.JAVA_11)
-    void testJRE11() {
-    }
-
-    @Test
-    @DisabledOnJre(JRE.JAVA_16)
-    void testNOJRE15() {
-    }
-
-    @Test
-    @EnabledIfSystemProperty(named = "java.version", matches = "1.8.0")
-    void testJavaVersion() {
-    }
-
-    @Test
-    @EnabledIfSystemProperty(named = "user.name", matches = "peddoooo")
-    void testUsuario() {
-    }
-
-    @Test
-    @EnabledIfSystemProperty(named = "os.arch", matches = ".*64.*")
-    void testArquitectura() {
-    }
-
-    @Test
-    @EnabledIfSystemProperty(named = "ENV", matches = "dev")
-    void testDev() {
-    }
-
-    @Test
-    void imprimirSystemProperties() {
-        var properties = System.getProperties();
-        properties.forEach((k, v) -> System.out.println(k + ":" + v));
-    }
-
-    @Test
-    void imprimirVariablesEntorno() {
-        var getenv = System.getenv();
-        getenv.forEach((k, v) -> System.out.println(k + ":" + v));
-    }
-
-    @Test
-    @EnabledIfEnvironmentVariable(named = "COMPUTERNAME", matches = "ASUS")
-    void testNombrePC() {
+        @Test
+        @EnabledIfEnvironmentVariable(named = "NUMBER_OF_PROCESSORS", matches = "7")
+        void testNumeroProcesadores() {
+        }
     }
 
 //    Uso de assumptions para validar si dentro de un metodo se cumple una condicion
